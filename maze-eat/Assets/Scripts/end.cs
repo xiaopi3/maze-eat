dd6934ff6079209ba707aaccef02abbc09609e78
xiaopi3 <33209712+xiaopi3@users.noreply.github.com>
2017-11-21 16:49:55 +0800

    finish

---
+++ maze-eat/Assets/Scripts/end.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class end : MonoBehaviour {
+    public GameObject plane1;
+    public GameObject plane2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.DetachChildren();
+        Destroy(other.GetComponent<move>());
+        Destroy(transform.gameObject);
+        Destroy(plane1);
+        Destroy(plane2);
+        Destroy(other.GetComponent<CharacterController>());
+        other.gameObject.AddComponent<Rigidbody>();
+    }
+}
---
+++ maze-eat/Assets/Scripts/move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    public float speed = 1.0f;
+    CharacterController cc;
+	// Use this for initialization
+	void Start () {
+        cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+            cc.SimpleMove(Vector3.forward * speed);
+        else if (Input.GetKey(KeyCode.S))
+            cc.SimpleMove(Vector3.back * speed);
+        else if (Input.GetKey(KeyCode.A))
+            cc.SimpleMove(Vector3.left * speed);
+        else if (Input.GetKey(KeyCode.D))
+            cc.SimpleMove(Vector3.right * speed);
+	}
+}
